b9ac7e97e2e13d4eb0a88a8cd25851fc3663bafe
minminzemi-cat <2511005@i-seifu.jp>
2025-11-11 11:31:54 +0900

    CBossを作成

---
+++ ACT/SourceCode/CBoss.cpp
@@ -0,0 +1,17 @@
+#include "CBoss.h"
+
+CBoss::CBoss()
+{
+}
+
+CBoss::~CBoss()
+{
+}
+
+void CBoss::Update()
+{
+}
+
+void CBoss::Draw(CCamera* pCamera)
+{
+}
---
+++ ACT/SourceCode/CBoss.h
@@ -0,0 +1,22 @@
+#pragma once
+#include"CCharacter.h"		//キャラクタークラス
+
+/**********************************
+* 		ボスクラス
+*	***/
+class CBoss
+	: public CCharacter  //キャラクタークラスを継承
+{
+	public:
+		CBoss();//コンストラクタ
+		~CBoss() override ;//デストラクタ
+		
+		//動作関数
+		void Update() override;//override:再定義前の関数が仮想関数かチェックできる
+								//		仮想関数でない場合はエラーになる
+		//描画関数
+		void Draw(CCamera* pCamera) override;
+private:
+
+
+};
